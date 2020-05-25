23e82675a01da9c408b2c5e590e027a77318bdc3
SteveB <steven.bull.sb@googlemail.com>
2020-05-25 23:06:39 +0100

    Added Application window support to the Desktop Launcher file
    Signed-off-by: SteveB <steven.bull.sb@googlemail.com>

--- desktop/src/com/mygdx/acechaplin/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/acechaplin/desktop/DesktopLauncher.java
@@ -1,8 +1,9 @@
 package com.mygdx.acechaplin.desktop;
 
+import com.badlogic.gdx.Application;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-
 import com.mygdx.acechaplin.AceChaplinGame;
 
 public class DesktopLauncher {
